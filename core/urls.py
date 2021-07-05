ee8f7724717e747d42cc706af4744dce30399a77
DiegRomerov <dieg.romerov@duocuc.cl>
2021-07-05 15:36:55 -0400

    version2.6.2
    agregadas todas las paginas de productos

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,18 @@
-from core.views import home,mb
+from core.views import home,mb,cpu,gpu,psu,ram,hdd,ssd,m2
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('', home, name='index'),
-    path('mb/', mb, name='mb')
+    path('mb/', mb, name='mb'),
+    path('cpu/', cpu, name='cpu'),
+    path('gpu/', gpu, name='gpu'),
+    path('psu/', psu, name='psu'),
+    path('ram/', ram, name='ram'),
+    path('hdd/', hdd, name='hdd'),
+    path('ssd/', ssd, name='ssd'),
+    path('m2/', m2, name='m2')
 ]
 
 urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- core/views.py
+++ core/views.py
@@ -9,6 +9,34 @@ def mb (request):
     data = {}
     return render(request, 'mb.html', data)
 
+def cpu (request):
+    data = {}
+    return render(request, 'cpu.html', data)
+
+def gpu (request):
+    data = {}
+    return render(request, 'gpu.html', data)
+
+def psu (request):
+    data = {}
+    return render(request, 'psu.html', data)
+
+def ram (request):
+    data = {}
+    return render(request, 'ram.html', data)
+
+def hdd (request):
+    data = {}
+    return render(request, 'hdd.html', data)
+
+def ssd (request):
+    data = {}
+    return render(request, 'ssd.html', data)
+
+def m2 (request):
+    data = {}
+    return render(request, 'm2.html', data)
+
     """
     agregar
 
